efa494355ab166fa13e2b2228eb71d46134b8c0d
hwater27 <dltjdtn0727@gmail.com>
2023-06-30 18:52:34 +0900

    On branch branchStudy1  Changes to be committed: 	modified:   src/test/java/Sample.java

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
     public static void main(String[] args){
         System.out.println("Hello, Project~~~~");
         System.out.println("저는 김수민 ㅎㅎ");
+        System.out.println("Branch from hwater27");
     }
 }
