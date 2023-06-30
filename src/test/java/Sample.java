4991ec6bf31e1109f17fc56c6acdf10e9cc1ea9e
sootreeah <suminsuminkim0518@naver.com>
2023-06-30 18:47:54 +0900

    edited by sumin

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -3,5 +3,6 @@ package test.java;
 public class Sample {
     public static void main(String[] args){
         System.out.println("Hello, Project!");
+        System.out.println("저는 김수민 ㅎㅎ");
     }
 }
