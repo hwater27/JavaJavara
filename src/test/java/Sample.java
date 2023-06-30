097bb107fcf1e6f8214c1c0278b32878e1a93642
sootreeah <suminsuminkim0518@naver.com>
2023-06-30 19:01:35 +0900

    sumin's brance

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -5,5 +5,7 @@ public class Sample {
         System.out.println("Hello, Project~~~~");
         System.out.println("저는 김수민 ㅎㅎ");
         System.out.println("Branch from hwater27");
+        System.out.println("쓸데없는 말");
+        System.out.println("추가헤주세용");
     }
 }
