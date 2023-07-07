d5dc476902342a3b716da471ec11bf45f49754f7
jinnykj <jinnykj@snu.ac.kr>
2023-07-07 19:12:50 +0900

    규진추가3

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -13,6 +13,8 @@ public class Sample {
         System.out.println("추가22222222");
         System.out.println("김수민 추가합니달라");
         System.out.println("또추가");
+        System.out.println("또추가함");
+        System.out.println("또33333");
     }
 }
 
