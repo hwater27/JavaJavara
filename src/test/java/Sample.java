f7da5a788ab7a027e07a181aa9ff5534635891d6
jinnykj <jinnykj@snu.ac.kr>
2023-07-07 18:12:34 +0900

    출력 코드 추가

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -7,5 +7,9 @@ public class Sample {
         System.out.println("Branch from hwater27");
         System.out.println("쓸데없는 말");
         System.out.println("추가헤주세용");
+        System.out.println("안녕하세용~~~~~~~^.^");
+        
     }
 }
+
+
