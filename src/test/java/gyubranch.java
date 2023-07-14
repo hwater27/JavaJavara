236967bd584e88dc5d168d1a0b3e721ca807dd17
jinnykj <jinnykj@snu.ac.kr>
2023-07-15 02:58:03 +0900

    스케줄정리

--- src/test/java/gyubranch.java
+++ src/test/java/gyubranch.java
@@ -1,5 +1,87 @@
 package test.java;
 
+import jave.util.*;
+
 public class gyubranch {
-    System.out.println("규브랜치");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("<메뉴>");
+        System.out.println("1. 스케줄 추가");
+        System.out.println("2. 스케줄 조회");
+        System.out.println("3. 종료");
+
+        while (true) {
+            System.out.print("\n메뉴를 선택하세요: ");
+            int choice1 = sc.nextInt();
+
+            switch (choice1) {
+                case 1: //스케줄 추가
+                    scanner.nextLine(); 
+                    System.out.print("스케줄 제목: ");
+                    String title = sc.nextLine();
+                    System.out.print("날짜 (yyyy-MM-dd): ");
+                    String date = sc.nextLine();
+                    System.out.print("시간: ");
+                    String time = sc.nextLine();
+                    
+                    
+
+                    
+                    break;
+                    
+                   
+                case 2: //스케줄 조회
+                    scanner.nextLine(); 
+                    System.out.print("조회할 날짜 (yyyy-MM-dd): ");
+                    String searchDate = sc.nextLine();
+                    
+                    
+                    while (true) {
+                    	System.out.print("\n메뉴를 선택하세요:");
+                    	System.out.println("1. 스케줄 수정");
+                    	System.out.println("2. 스케줄 삭제");
+                    	System.out.println("3. 첫 화면으로 돌아가기 ");
+                    	
+                    	int choice2 = sc.nextInt();
+                    	
+                    	if(choice2 == 1) { //스케줄 수정
+                    		
+                    	}
+                    	
+                    	if(choice2 == 2) { //스케줄 삭제
+                    		System.out.print("삭제할 스케줄 번호: ");
+                            int Index = sc.nextInt();
+
+                            System.out.println("스케줄이 삭제 되었습니다.");
+                          
+                    		
+                    	}
+                    	if(choice2 ==3) { //첫 화면
+                    		break;
+                    	}
+                    	
+                    	else { //그 외 숫자
+                    		System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                    	}
+                    			
+                    	
+                    	
+                    }
+                    break;
+               
+            
+                case 3: //프로그램 종료
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                    
+                default: //그 외 숫자
+                    System.out.println("메뉴를 다시 선택해주세요.");
+                    break;
+            }
+        }
+    }
+}
+
+    
 }
